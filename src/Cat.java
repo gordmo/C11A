c44cfce144c5b92d1ff5185992d211cbe5b0872d
SOFIA ANCHIA <SANCHIA@mines.com>
2024-02-16 16:49:49 -0700

    Fixed Cat and helped add ears

--- src/Cat.java
+++ src/Cat.java
@@ -1,49 +1,106 @@
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.*;
-import java.awt.geom.*;
 
 public class Cat {
-	// drawing constants are private - noone needs to know what we're doing
-	// pick a head dimension
-	private static final int HEAD_DIMENSION = 100;
-	// eyes will be about 1/4 from top of head and 1/4 from left
-	private static final int EYE_Y = HEAD_DIMENSION/4;
-	private static final int EYE_X = HEAD_DIMENSION/4;
-	private static final int EYE_SEPARATION = HEAD_DIMENSION/3;
-	// pick eye dimensions
-	private static final int EYE_HEIGHT = 20;
-	private static final int EYE_WIDTH = 10;
-	// pick mouth height, width is based on head dimension
-	private static final int MOUTH_HEIGHT = 10;
-	private static final int MOUTH_WIDTH = HEAD_DIMENSION/4;
-	// mouth starts about 40% from left edge of head
-	private static final int MOUTH_X = HEAD_DIMENSION/5 * 2;
-	private static final int MOUTH_Y = HEAD_DIMENSION/5 * 3;
-	
-	// draw will render the Cat on the Graphics object
-	public void draw(Graphics g, int catX, int catY)
-	{
-		Graphics2D g2 = (Graphics2D) g;
-		int x=catX;
-		int y=catY;
-		// Draw the head
-		g2.setColor(Color.black);
-		g2.fillOval(x, y, HEAD_DIMENSION, HEAD_DIMENSION);
-		// Draw the eyes
-		g2.setColor(Color.green);
-		x = catX + EYE_X; 
-		y = catY + EYE_Y;
-		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
-		x += EYE_SEPARATION;
-		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
-		// Draw the mouth
-		g2.setColor(Color.pink);
-		x = catX + MOUTH_X;
-		y = catY + MOUTH_Y;
-		g2.fillOval(x, y, MOUTH_WIDTH, MOUTH_HEIGHT);
-		g2.setColor(Color.black);
-		// Meow text appears below cat head, +10 places below 
-		// so it doesn't overlap the drawing
-		g2.drawString("Meow", catX, catY+HEAD_DIMENSION+10);	
-	}
+    private int headDimension;
+    private int eyeY;
+    private int eyeX;
+    private int eyeSeparation;
+    private int eyeHeight;
+    private int eyeWidth;
+    private int mouthHeight;
+    private int mouthWidth;
+    private int mouthX;
+    private int mouthY;
+
+    public Cat(int headDimension, int eyeY, int eyeX, int eyeSeparation,
+               int eyeHeight, int eyeWidth, int mouthHeight, int mouthWidth,
+               int mouthX, int mouthY) {
+        this.headDimension = headDimension;
+        this.eyeY = eyeY;
+        this.eyeX = eyeX;
+        this.eyeSeparation = eyeSeparation;
+        this.eyeHeight = eyeHeight;
+        this.eyeWidth = eyeWidth;
+        this.mouthHeight = mouthHeight;
+        this.mouthWidth = mouthWidth;
+        this.mouthX = mouthX;
+        this.mouthY = mouthY;
+    }
+
+
+    public void draw(Graphics g, int catX, int catY) {
+        Graphics2D g2 = (Graphics2D) g;
+
+        drawHead(g2, catX, catY);
+        drawEyes(g2, catX, catY);
+        drawMouth(g2, catX, catY);
+
+        // Meow text appears below cat head, +10 places below
+        // so it doesn't overlap the drawing
+        g2.setColor(Color.black);
+        g2.drawString("Meow", catX, catY + headDimension + 10);
+    }
+
+    // Method to draw the cat's head
+    private void drawHead(Graphics2D g2, int catX, int catY) {
+        g2.setColor(Color.black);
+        g2.fillOval(catX, catY, headDimension, headDimension);
+
+        // Draw ears
+        int earWidth = headDimension / 3;
+        int earHeight = headDimension / 2;
+        int earX1 = catX - earWidth / 2;
+        int earX2 = catX + headDimension - earWidth / 2;
+        int earY = catY - earHeight / 2;
+        
+        Polygon leftEar = new Polygon();
+        leftEar.addPoint(earX1, earY);
+        leftEar.addPoint(earX1 + earWidth, earY);
+        leftEar.addPoint(earX1 + earWidth / 2, earY - earHeight);
+        
+        Polygon rightEar = new Polygon();
+        rightEar.addPoint(earX2, earY);
+        rightEar.addPoint(earX2 + earWidth, earY);
+        rightEar.addPoint(earX2 + earWidth / 2, earY - earHeight);
+        
+        g2.fillPolygon(leftEar);
+        g2.fillPolygon(rightEar);
+    }
+
+    // Method to draw the cat's eyes
+    private void drawEyes(Graphics2D g2, int catX, int catY) {
+        g2.setColor(Color.green);
+        int x = catX + eyeX;
+        int y = catY + eyeY;
+        g2.fillOval(x, y, eyeWidth, eyeHeight);
+        x += eyeSeparation;
+        g2.fillOval(x, y, eyeWidth, eyeHeight);
+    }
+
+    // Method to draw the cat's mouth
+    private void drawMouth(Graphics2D g2, int catX, int catY) {
+        g2.setColor(Color.pink);
+        int x = catX + mouthX;
+        int y = catY + mouthY;
+        g2.fillOval(x, y, mouthWidth, mouthHeight);
+    }
+
+    public static void main(String[] args) {
+        // Example usage:
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 300);
+        frame.add(new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                Cat cat = new Cat(100, 25, 25, 33, 20, 10, 10, 25, 40, 60);
+                cat.draw(g, 100, 100);
+            }
+        });
+        frame.setVisible(true);
+    }
 }
+
